70de0531c22cdbaaf2377ff574e07bdc609d5536
patilashishp <109000399+patilashishp@users.noreply.github.com>
2022-07-29 16:53:53 +0530

    Update TestGreeter.java

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -30,4 +30,74 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  
+  @Test
+  public void greetShouldIncludeTheOneBeingGreeted1() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone), containsString(someone));
+  }
+
+  @Test
+  public void greetShouldIncludeGreetingPhrase1() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+  
+  @Test
+  public void greetShouldIncludeTheOneBeingGreeted2() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone), containsString(someone));
+  }
+
+  @Test
+  public void greetShouldIncludeGreetingPhrase2() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+  
+  @Test
+  public void greetShouldIncludeTheOneBeingGreeted3() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone), containsString(someone));
+  }
+
+  @Test
+  public void greetShouldIncludeGreetingPhrase3() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+  
+  @Test
+  public void greetShouldIncludeTheOneBeingGreeted4() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone), containsString(someone));
+  }
+
+  @Test
+  public void greetShouldIncludeGreetingPhrase4() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+  
+  @Test
+  public void greetShouldIncludeTheOneBeingGreeted5() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone), containsString(someone));
+  }
+
+  @Test
+  public void greetShouldIncludeGreetingPhrase5() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
